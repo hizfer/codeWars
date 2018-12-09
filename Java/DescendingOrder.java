51cd4f4491b9a8a534ab2d3d7ebe6fb096c31b23
hizfer <hizfernandez@gmail.com>
2018-12-09 19:36:39 +0100

    Update DescendingOrder.java

--- Java/DescendingOrder.java
+++ Java/DescendingOrder.java
@@ -2,7 +2,7 @@
 
 /*
 
-Your task is to make a function that can take any non-negative integer as a argument and return
+Your task is to make a function that can take any non-negative integer as an argument and return
 it with it's digits in descending order. 
 Essentially, rearrange the digits to create the highest possible number.
 
